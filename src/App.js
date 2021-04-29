162f5ed5e17a6f25b0a06d318abe4ebbba0009c7
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-04-29 16:30:14 -0700

    Step 11

--- src/App.js
+++ src/App.js
@@ -70,6 +70,32 @@ const App = () => {
         <p>
           Welcome to the app!
         </p>
+        <svg className="map" viewBox="-180 -90 360 180">
+          <g transform="scale(1 -1)">
+            <image xlinkHref="equirectangular_world.jpg" href="equirectangular_world.jpg" x="-180" y="-90" height="100%" width="100%" transform="scale(1 -1)"/>
+            
+            {filteredAirlines.map(route => {
+              const src = route.src;
+              const dest = route.dest;
+              const srcAirport = getAirportByCode(src);
+              const destAirport = getAirportByCode(dest);
+              const [x1, y1, x2, y2] = [srcAirport.long, srcAirport.lat, destAirport.long, destAirport.lat];
+              return (
+                <g key="">
+                  <circle className="source" cx={x1} cy={y1}>
+                    <title>{srcAirport.name}</title>
+                  </circle> 
+                  <circle className="destination" cx={x1} cy={y1}>
+                    <title>{destAirport.name}</title>
+                  </circle>
+                  <path d={`M${x1} ${y1} L ${x2} ${y2}`} />
+                </g>
+              );
+            })}
+            
+          </g>
+        </svg>
+        <br></br>
         Show routes on
         <Select options={data.airlines} valueKey="id" titleKey="name"
           allTitle="All Airlines" value="" onSelect={onSelect} selectedAirline={selectedAirline} includedData={includedData}/>
